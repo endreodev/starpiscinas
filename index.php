c388d81b87a90ba569be38e942e4671a6b3cd3a7
endreodev <endreo.dev@gmail.com>
2026-02-02 01:16:01 -0300

    fix: resolve 404 error by adding root index.php and updating deploy script

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * This file allows us to run the application from the root directory.
+ * It bridges the request to the public/index.php file.
+ */
+
+require_once __DIR__ . '/public/index.php';
